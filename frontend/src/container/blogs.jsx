add718f1b81a3faf8b8c201505996c0913cef15a
Pixelated-Sage <thepixelatedsage@gmail.com>
2025-08-31 19:55:28 +0530

    changes

--- frontend/src/container/blogs.jsx
+++ frontend/src/container/blogs.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { motion, AnimatePresence } from "framer-motion";
 import { fetchBlogs, fetchBlogBySlug } from "../lib/api";
-import Sample from "../../../public/assets/img/dsat.jpg"
+import Sample from "../../public/assets/img/dsat.jpg"
 // --- MOTION CONFIG ---
 const fadeIn = {
   hidden: { opacity: 0, y: 20 },
